b889b06f54284ea95fdcd2eb864135aebe5c31ba
Yicheng Wang <alex.wyc2098@protonmail.ch>
2016-02-26 15:48:58 -0500

    updated

--- main.cpp
+++ main.cpp
@@ -81,4 +81,24 @@ int main() {
     screen.draw_edge_set(c, es);
 
     screen.save_ppm("line.ppm");
+
+    float I[4][4] = {
+        {1, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1}
+    };
+
+    float A[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    float B[4][4];
+
+    matrix_multiply_4(B, I, A);
+
+    print_matrix_4(B);
 }
